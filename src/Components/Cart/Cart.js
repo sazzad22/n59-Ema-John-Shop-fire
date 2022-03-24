bdb238ef0a4e758b7baf19bfea399bf36600325f
Sazzad ur Rahman <22joylobo@gmail.com>
2022-03-24 10:28:50 +0600

    cart calculation

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 const Cart = (props) => {
-    console.log(props.cart);
+
+    const { cart } = props;
+
+
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+
+    }
+    // console.log(cart);
+    const tax = (total * 0.1).toFixed(2);
+    console.log(tax);
+    
+
+
+    
     return (
-        <div>
-            <h4>Cart</h4>
-            <p></p>
+        <div className='cart'>
+            <h4>Order Summary</h4>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${ total }</p>
+            <p>Total Shipping Charge: ${ shipping }</p>
+            <p>Tax: ${ tax }</p>
+            <p>Grand Total: $</p>
         </div>
     );
 };
