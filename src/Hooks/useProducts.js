6f3de8125b6fa469f63115fb590ff28af0a18760
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-28 14:31:44 +0600

    db setup

--- src/Hooks/useProducts.js
+++ src/Hooks/useProducts.js
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const useProducts = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-        
-    },[]);
-    return [products, setProducts]
-    //a fucntion in js can return only one single thing, to return multiple we use array. 
-}
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/product")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return [products, setProducts];
+  //a fucntion in js can return only one single thing, to return multiple we use array.
+};
 export default useProducts;
